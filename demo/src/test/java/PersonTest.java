7a66f0e133bf1b2ecbe965a0b10c55af9854ce7f
Vittaaa <s4061620@student.rmit.edu.au>
2025-05-28 23:28:48 +1000

    test-case(5) for addDemerits function

--- demo/src/test/java/PersonTest.java
+++ demo/src/test/java/PersonTest.java
@@ -48,4 +48,54 @@ public class PersonTest {
         boolean result = person.addPerson("56!@abc#CD", "123|Main St|Melbourne|Victoria|Australia", "1990-11-15", testFile);
         assertFalse(result);
     }
+
+    //test-case for addDemeritPoints
+
+    @Test
+    void testValidAdultNoSuspension() {
+        Person person = new Person();
+        boolean result = person.addPerson("57s_d@%fDA", "123|Main St|Melbourne|Victoria|Australia", "15-11-1990", testFile);
+        assertTrue(result);
+        String result2 = person.addDemeritPoints("57s_d@%fDA", 3, "10-05-2024", testFile);
+        assertEquals("Success", result2);
+        assertFalse(person.isSuspended());
+    }
+
+    @Test
+    void testInvalidDateFormat() {
+        Person person = new Person();
+        boolean result = person.addPerson("57s_d@%fDA", "123|Main St|Melbourne|Victoria|Australia", "15-11-1991", testFile);
+        assertTrue(result);
+        String result2 = person.addDemeritPoints("57s_d@%fDA", 3, "2024-05-10", testFile); 
+        assertEquals("Failed", result2);
+    }
+
+    @Test
+    void testPointsOutOfRange() {
+        Person person = new Person();
+        boolean result = person.addPerson("57s_d@%fDA", "123|Main St|Melbourne|Victoria|Australia", "15-11-1992", testFile);
+        assertTrue(result);
+        String result2 = person.addDemeritPoints("57s_d@%fDA", 7, "10-05-2024", testFile); 
+        assertEquals("Failed", result2);
+    }
+
+    @Test
+    void testUnderageSuspension() {
+        Person person = new Person();
+        boolean result = person.addPerson("57s_d@%fDA", "123|Main St|Melbourne|Victoria|Australia", "15-11-2023", testFile);
+        assertTrue(result);
+        person.addDemeritPoints("57s_d@%fDA", 3, "01-05-2024", testFile);
+        person.addDemeritPoints("57s_d@%fDA", 4, "10-05-2024", testFile); 
+        assertTrue(person.isSuspended());
+    }
+
+    @Test
+    void testWrongPersonID() {
+        Person person = new Person();
+        boolean result = person.addPerson("57s_d@%fDA", "123|Main St|Melbourne|Victoria|Australia", "15-11-1993", testFile);
+        assertTrue(result);
+        String result2 = person.addDemeritPoints("WRONGID", 3, "10-05-2024", testFile);
+        assertEquals("Failed", result2);
+    }       
+
 }
